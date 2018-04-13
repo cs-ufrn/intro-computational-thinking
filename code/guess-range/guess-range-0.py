cc2c99d5c2d84987570a0cc088b92f841e72a9f3
Vitor Greati <greati@ufrn.edu.br>
2018-04-13 12:24:28 -0300

    Update code

--- code/guess-range/guess-range-0.py
+++ code/guess-range/guess-range-0.py
@@ -1,15 +1,15 @@
 from random import randint
 
-print("\n\nO computador vai pensar em um número de 1 a 100.\nTente adivinhar que número é esse!\nAtenção nas dicas!\n\n")
+print("\n\nO computador vai pensar em um número de 1 a 10.\nTente adivinhar que número é esse!\n\n")
 
-resposta = randint(1,101)
+resposta = randint(1,11)
 
 tentativa = int(input("Qual o seu chute? "))
 
 if (tentativa > resposta):
-    print("Tente um número menor!\n")
+    print("Errou! Era um número menor!\n")
 elif (tentativa < resposta):
-    print("Tente um número maior!\n")
+    print("Errou! Era um número maior!\n")
 else:
-    print("Você acertou!")
+    print("Você acertou!\n")
 
